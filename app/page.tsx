9e116d7581f6cd67e5c3e49308e95bfb386adeac
Fahrul Hamzidan Pulungan <145125810+z1dan820@users.noreply.github.com>
2026-07-13 01:13:02 +0800

    Replace main content with Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-
-      <main className="min-h-screen bg-black text-white flex items-center justify-center">
-        <h1 className="text-7xl font-bold">
-          HAZI
-        </h1>
-      </main>
+      <Hero />
     </>
   );
 }
