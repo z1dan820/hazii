4e01ecf1673906f02438c891bd83e1d3123b5eb0
Fahrul Hamzidan Pulungan <145125810+z1dan820@users.noreply.github.com>
2026-07-13 01:17:20 +0800

    Add Services component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Services />
     </>
   );
 }
