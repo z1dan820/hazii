66c7b41d67b43f6e4fcce1b304275ba2dbcbc37d
Fahrul Hamzidan Pulungan <145125810+z1dan820@users.noreply.github.com>
2026-07-12 18:43:39 +0800

    Import Michroma font in layout.tsx
    Add Michroma font import for layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Michroma } from "next/font/google";
+
+const michroma = Michroma({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
